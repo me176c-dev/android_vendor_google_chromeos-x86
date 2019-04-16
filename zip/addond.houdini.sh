a94d194a46baed210c3c71a5ec91ff37d71c2d90
lambdadroid <lambdadroid@gmail.com>
2019-04-16 23:02:31 +0200

    zip: addond.houdini.sh: Escape '.' in regex

--- zip/addond.houdini.sh
+++ zip/addond.houdini.sh
@@ -5,7 +5,7 @@ case "$1" in
   post-restore)
     # Add ARM to the list of supported ABIs (if it is not listed already)
     # Extra system properties are always added, it does not matter if they are listed twice
-    sed -ri -e '/^ro.product.cpu.abilist(32)?=/ {/armeabi/! s/$/,armeabi-v7a,armeabi/}' \
+    sed -ri -e '/^ro\.product\.cpu\.abilist(32)?=/ {/armeabi/! s/$/,armeabi-v7a,armeabi/}' \
         -e '$ a # Houdini\nro.product.cpu.abi2=armeabi-v7a\nro.dalvik.vm.isa.arm=x86\nro.enable.native.bridge.exec=1' \
         /system/build.prop
   ;;
